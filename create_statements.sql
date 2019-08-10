d1d9db6176bc35a19b7afefcb9ef07499811b3ae
Ashley Schenker <47718194+aktavern@users.noreply.github.com>
2019-08-10 10:52:50 -0500

    final create statements and notebook

--- create_statements.sql
+++ create_statements.sql
@@ -15,3 +15,20 @@ CREATE TABLE outcomes(id varchar(30),
                        outcome_date timestamp,
 					  outcome_type varchar,
                        outcome_age varchar)
+
+
+CREATE TABLE intakes_2019(id varchar(30),
+             intake_name varchar,
+             intake_date timestamp,
+              intake_type varchar,
+              intake_condition varchar,
+              animal_type varchar,
+             intake_age varchar,
+             breed varchar,
+             color varchar);
+			 
+CREATE TABLE outcomes_2019(id varchar(30),
+                       outcome_name varchar,
+                       outcome_date timestamp,
+					  outcome_type varchar,
+                       outcome_age varchar)
